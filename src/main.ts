c14276c43e8b6f66bc15f79860d8c4a87082d9c5
peerplay-network <peerplay.network@gmail.com>
2023-04-08 12:36:45 +0200

    Setup RootDB Server

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+const PouchDB = require("pouchdb");
+PouchDB.plugin(require("comdb"));
+const database_list = ["nodes","accounts"];
+let databases = [];
+console.log("Peerplay DB Server Starting");
+const password = process.argv[2] || "" // Set Default Password here or pass it as an argumen
+database_list.forEach((db_name) => {
+  console.log(`Starting Database ${db_name}`);
+  const database = new PouchDB(db_name);
+  database.setPassword(password);
+  databases.push(database);
+  console.log(`Started Database ${db_name}`);
+});
+
+console.log("Peerplay DB Server Started on Port 5984");
